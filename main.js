625c3aa4bd7f76d67a62f39a861fe6806bd2381b
Anton <antonkosicyn@rambler.ru>
2021-07-18 01:16:12 +0300

    fix:hide test function

--- main.js
+++ main.js
@@ -56,11 +56,11 @@ function updateMousePos(evt) {
     paddleX = mouseX - PADDLE_WIDTH / 2
 
 
-    arrayOfBalls[0].x = mouseX
-    arrayOfBalls[0].y = mouseY
-    arrayOfBalls[0].xSpeed = 8
-    arrayOfBalls[0].ySpeed = -8
-        //what is happening???
+    // arrayOfBalls[0].x = mouseX
+    // arrayOfBalls[0].y = mouseY
+    // arrayOfBalls[0].xSpeed = 8
+    // arrayOfBalls[0].ySpeed = -8
+    //what is happening???
 
 }
 
